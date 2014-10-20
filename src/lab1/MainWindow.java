b91af9d25bd42e4bf73a082fc1a7e6829872b08b
rustedarrows <ch4nger@gmail.com>
2014-10-20 11:45:57 -0500

    initial

---
+++ src/lab1/MainWindow.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author Timothy
+ */
+public class MainWindow extends JFrame{
+    private JLabel prompt, output;
+    private JTextField input;
+    private JButton sayHello;
+    
+    
+}
